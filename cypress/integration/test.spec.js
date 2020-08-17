0c58cb75945f7c82861762b71d185a00f348fd46
despi <despi@LAPTOP-JL8GAFLU>
2020-08-17 15:30:18 +0200

    Added hooks

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,8 +1,11 @@
 /// <reference types="cypress" />
+
 describe("Account creation", () => {
-    it('should create a new account for the ecommerce website', () => {
-        // Visit website
-       cy.visit('https://www.demoblaze.com/index.html')
+    before(() => {
+        cy.log('Tests have been started')
+    })
+    beforeEach(() => {
+        cy.visit('https://www.demoblaze.com/index.html')
     
         // Click on the sign up button
         cy.get('#signin2').click()
@@ -25,7 +28,9 @@ describe("Account creation", () => {
         // Write the username and the password
         cy.get('#sign-username').type(generatedUsername)
         cy.get('#sign-password').type('randompassword')
-    
+    })
+    it('should create a new account for the ecommerce website', () => {
+
         // Click on the Sign up button
         cy.get('button').contains('Sign up').click()
     
@@ -36,36 +41,14 @@ describe("Account creation", () => {
     })
     
     it('should cancel creating the new account for the ecommerce website', () => {
-        // Visit website
-       cy.visit('https://www.demoblaze.com/index.html')
-    
-        // Click on the sign up button
-        cy.get('#signin2').click()
-    
-        // Verify sign up page
-        cy.get('#signInModalLabel').contains('Sign up')
-    
-        cy.wait(2000)
-        // Function that generate a new username with each new test
-        function generateNewUsername() {
-            let text = "";
-            let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    
-            for(let i = 0; i < 10; i++) 
-            text += alphabet.charAt(Math.floor(Math.random() * alphabet.length))
-            return text;
-        }
-    
-        const generatedUsername = generateNewUsername()
-        // Write the username and the password
-        cy.get('#sign-username').type(generatedUsername)
-        cy.get('#sign-password').type('randompassword')
-    
+      
         // Click on the close button
         cy.get('#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-secondary').click()
         // Verify the sign up page is not visible
         cy.get('#signInModalLabel').should('not.be.visible')
     })
-    
+    afterEach(() => {
+        cy.log('Test has succeeded.')
+    })
 })
 
